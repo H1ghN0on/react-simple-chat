3c9bcd7dc45c3a5ed2944d7fc85daac091198ac9
h1ghn0on <you@example.com>
2021-02-04 16:43:37 +0300

    adding messages, online list

--- server.js
+++ server.js
@@ -5,15 +5,61 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server,{cors:{origin:"*"}})
 
+app.use(express.json());
+app.use(express.urlencoded( { extended: true }));
+
 const rooms = new Map();
 
-app.get('/rooms', (req, res) => {
-    res.json(rooms);
+app.get('/rooms/:id', (req, res) => {
+    const { id: roomId } = req.params;
+    const obj = rooms.has(roomId) ? {
+        users: [...rooms.get(roomId).get('users').values()],
+        messages: [...rooms.get(roomId).get('messages').values()],
+    } : { users: [], messages: []};
+    res.json(obj);
 })
 
+
+app.post('/rooms', (req, res) => {
+    const { roomId, username } = req.body;
+    if (!rooms.has(roomId)) {
+        rooms.set(roomId, new Map([
+            ['users', new Map()],
+            ['messages', []],
+        ]));
+    }
+    res.send();
+});
+
 io.on('connection', socket => {
+    socket.on('ROOM:JOIN', ({ roomId, username }) => {
+        socket.join(roomId);
+        rooms.get(roomId).get('users').set(socket.id, username);
+        const users = [...rooms.get(roomId).get('users').values()];
+        socket.to(roomId).broadcast.emit('ROOM:SET_USERS', users);
+    })
     console.log('user connected', socket.id);
+
+    socket.on('ROOM:NEW_MESSAGE', ({ roomId, username, text }) => {
+        const obj = {
+            username,
+            text,
+        }
+        rooms.get(roomId).get('messages').push(obj);
+        socket.to(roomId).broadcast.emit('ROOM:NEW_MESSAGE', obj);
+    } )
+
+    socket.on('disconnect', () => {
+        rooms.forEach((value, roomId) => {
+            if (value.get('users').delete(socket.id)) {
+                const users = [...rooms.get(roomId).get('users').values()];
+                socket.to(roomId).emit('ROOM:SET_USERS', users);
+            };
+        })
+    })
 })
+
+
 
 server.listen(9999, (err) => {
     if (err) {
--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import socket from '../socket';
+import React from 'react';
+import axios from 'axios';
 
-function JoinBlock() {
+
+function JoinBlock({ onLogin }) {
+    const [roomId, setRoomId] = React.useState('');
+    const [username, setUsername] = React.useState('');
+    const [isLoading, setLoading] = React.useState(false);
+
+    const onEnter = async () => {
+        if (!roomId.trim() || !username.trim()) {
+            return alert('Неверный данные');
+        }
+        const obj = {
+            roomId,
+            username,
+        }
+        setLoading(true);
+        await axios
+        .post('/rooms', obj);
+        onLogin(obj);
+    }
     return (
         <div className="join-block">
-            <input type="text" placeholder="Room ID" value=""/>
-            <input type="text" placeholder="Ваше имя" value=""/>
-            <button className="btn btn-success">ВОЙТИ</button>
+            <input type="text" placeholder="Room ID" value={roomId} onChange={e => setRoomId(e.target.value)}/>
+            <input type="text" placeholder="Ваше имя" value={username} onChange={e => setUsername(e.target.value)}/>
+            <button 
+                disabled={isLoading}
+                onClick={onEnter} 
+                className="btn btn-success">
+                {isLoading ? "ВХОД..." : "ВОЙТИ"}
+            </button>
         </div>
     )
 }
